a1d23378fc44724b4c7521470f0cf1144d4d4c68
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-12-27 14:01:05 -0300

    Added dotenv crate for securing token

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod window;
 
+use dotenv::dotenv;
 use gtk::prelude::*;
 use gtk::{gio, glib, Application};
 use window::Window;
@@ -7,6 +8,8 @@ use window::Window;
 const APP_ID: &str = "juliangcalderon.youtube-downloader";
 
 fn main() -> glib::ExitCode {
+    dotenv().ok();
+
     gio::resources_register_include!("compiled.gresource").expect("Failed to register resources.");
 
     let app = Application::builder().application_id(APP_ID).build();

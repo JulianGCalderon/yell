87bb2a0f6a0d1ead8a7c31e773639250da6421d1
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-12-28 23:39:34 -0300

    finished feature

--- src/application_window/mod.rs
+++ src/application_window/mod.rs
@@ -1,5 +1,7 @@
 mod imp;
 
+use std::path::PathBuf;
+
 use gtk::gio::ListStore;
 use gtk::glib::spawn_future_local;
 
@@ -26,35 +28,6 @@ impl ApplicationWindow {
         Object::builder().property("application", app).build()
     }
 
-    async fn query_videos(&self) {
-        let buffer = self.imp().search_entry.buffer().text().to_string();
-        if buffer.is_empty() {
-            return;
-        }
-
-        let client = self.client();
-        let (sender, receiver) = async_channel::bounded(1);
-
-        RUNTIME.spawn(async move {
-            let res = client.query(buffer).await;
-            if let Err(err) = sender.send(res).await {
-                eprintln!("Could not send to channel, with error: {:?}", err);
-            }
-        });
-
-        match receiver.recv().await {
-            Ok(Ok(videos)) => {
-                self.set_results(videos.into());
-            }
-            Ok(Err(err)) => {
-                eprintln!("Could not query videos, with error: {:?}", err);
-            }
-            Err(err) => {
-                eprintln!("Could not receive from channel, with error: {:?}", err);
-            }
-        }
-    }
-
     fn set_results(&self, videos: Vec<VideoObject>) {
         self.results().remove_all();
         self.results().extend_from_slice(&videos);
@@ -73,111 +46,6 @@ impl ApplicationWindow {
             .item(selected_index)?
             .downcast::<VideoObject>()
             .ok()
-    }
-
-    async fn download_selected(&self) {
-        let selected = match self.get_selected() {
-            Some(selected) => selected,
-            None => return,
-        };
-
-        let id = selected.property("id");
-
-        let client = self.client();
-        let (sender, receiver) = async_channel::bounded(1);
-
-        RUNTIME.spawn(async move {
-            let res = client.download(id).await;
-            if let Err(err) = sender.send(res).await {
-                eprintln!("Could not send to channel, with error: {:?}", err);
-            }
-        });
-
-        match receiver.recv().await {
-            Ok(Ok(())) => {}
-            Ok(Err(err)) => {
-                eprintln!("Could not download video, with error: {:?}", err);
-            }
-            Err(err) => {
-                eprintln!("Could not receive from channel, with error: {:?}", err);
-            }
-        }
-    }
-
-    fn setup_callbacks(&self) {
-        self.imp()
-            .search_button
-            .connect_clicked(clone!(@weak self as self_ => move |_| {
-                spawn_future_local(async move {
-                    self_.imp().search_button.set_sensitive(false);
-                    self_.query_videos().await;
-                    self_.imp().search_button.set_sensitive(true);
-                });
-            }));
-
-        self.imp()
-            .search_entry
-            .connect_activate(clone!(@weak self as self_ => move |_| {
-                spawn_future_local(async move {
-                    self_.imp().search_button.set_sensitive(false);
-                    self_.query_videos().await;
-                    self_.imp().search_button.set_sensitive(true);
-                });
-            }));
-
-        self.imp()
-            .download_button
-            .connect_clicked(clone!(@weak self as self_ => move |_| {
-                let file_chooser_dialog = FileChooserDialog::new(
-                    Some("Select download location"),
-                    Some(&self_),
-                    FileChooserAction::Save,
-                    &[("Save", ResponseType::Accept), ("Cancel", ResponseType::Cancel)]
-                );
-
-                file_chooser_dialog.connect_response(clone!(@weak self_ => move |this, event| {
-                    match event {
-                        ResponseType::Accept => {
-                            let path = match this.file() {
-                                Some(path) => path,
-                                None => return,
-                            };
-
-                            spawn_future_local(async move {
-                                let download_dialog = Builder::from_resource(&format!("{}ui/download-dialog.ui", config::APP_IDPATH))
-                                    .objects().remove(0)
-                                    .downcast::<MessageDialog>()
-                                    .expect("Should be a MessageDialog");
-
-                                download_dialog.set_transient_for(Some(&self_));
-                                download_dialog.show();
-
-                                // refactor: get selection should happen before file chooser dialog
-                                self_.download_selected().await;
-
-                                let finished_dialog = Builder::from_resource(&format!("{}ui/finished-dialog.ui", config::APP_IDPATH))
-                                    .objects().remove(0)
-                                    .downcast::<MessageDialog>()
-                                    .expect("Should be a MessageDialog");
-
-                                finished_dialog.set_transient_for(Some(&self_));
-                                finished_dialog.connect_response(|self_, _| {
-                                    self_.destroy();
-                                });
-
-                                download_dialog.destroy();
-                                finished_dialog.show();
-                            });
-                        },
-                        _ => {},
-                    }
-
-                    this.destroy();
-                }));
-
-
-                file_chooser_dialog.present();
-            }));
     }
 
     fn setup_list(&self) {
@@ -205,5 +73,141 @@ impl ApplicationWindow {
 
     fn client(&self) -> Client {
         self.imp().client.borrow().clone()
+    }
+}
+
+#[gtk::template_callbacks]
+impl ApplicationWindow {
+    #[template_callback]
+    fn handle_search(&self) {
+        let query = self.imp().search_entry.buffer().text().to_string();
+        if query.is_empty() {
+            return;
+        }
+
+        spawn_future_local(clone!(@weak self as this => async move {
+            this.start_search(query).await;
+        }));
+    }
+
+    async fn start_search(&self, query: String) {
+        self.imp().search_button.set_sensitive(false);
+        self.query_videos(query).await;
+        self.imp().search_button.set_sensitive(true);
+    }
+
+    async fn query_videos(&self, query: String) {
+        let client = self.client();
+        let (sender, receiver) = async_channel::bounded(1);
+
+        RUNTIME.spawn(async move {
+            let res = client.query(query).await;
+            if let Err(err) = sender.send(res).await {
+                eprintln!("Could not send to channel, with error: {:?}", err);
+            }
+        });
+
+        match receiver.recv().await {
+            Ok(Ok(videos)) => {
+                self.set_results(videos.into());
+            }
+            Ok(Err(err)) => {
+                eprintln!("Could not query videos, with error: {:?}", err);
+            }
+            Err(err) => {
+                eprintln!("Could not receive from channel, with error: {:?}", err);
+            }
+        }
+    }
+
+    #[template_callback]
+    fn handle_download(&self) {
+        let selected = match self.get_selected() {
+            Some(selected) => selected,
+            None => return,
+        };
+
+        let selected_id = selected.property::<String>("id");
+
+        let file_chooser_dialog =
+            Builder::from_resource(&format!("{}ui/file-chooser-dialog.ui", config::APP_IDPATH))
+                .objects()
+                .remove(0)
+                .downcast::<FileChooserDialog>()
+                .expect("Should be a FileChooserDialog");
+
+        file_chooser_dialog.set_action(FileChooserAction::Save);
+        file_chooser_dialog.set_current_name(&format!("{}.mp4", selected_id));
+        file_chooser_dialog.set_transient_for(Some(self));
+
+        file_chooser_dialog.connect_response(clone!(@weak self as this => move |chooser, event| {
+            match event {
+                ResponseType::Cancel => chooser.destroy(),
+                ResponseType::Accept => {
+                    let path = match chooser.file().and_then(|file| file.path()) {
+                        Some(path) => path,
+                        None => return,
+                    };
+
+                    let selected_id = selected_id.clone();
+
+                    spawn_future_local(clone!( @weak this => async move {
+                        this.start_download(selected_id, path).await;
+                    }));
+
+                    chooser.destroy();
+                }
+                _ => {}
+            }
+        }));
+
+        file_chooser_dialog.present();
+    }
+
+    async fn start_download(&self, id: String, path: PathBuf) {
+        let download_dialog =
+            Builder::from_resource(&format!("{}ui/download-dialog.ui", config::APP_IDPATH))
+                .objects()
+                .remove(0)
+                .downcast::<MessageDialog>()
+                .expect("Should be a MessageDialog");
+
+        download_dialog.set_transient_for(Some(self));
+        download_dialog.show();
+
+        let client = self.client();
+        let (sender, receiver) = async_channel::bounded(1);
+
+        RUNTIME.spawn(async move {
+            let res = client.download(id, path).await;
+            if let Err(err) = sender.send(res).await {
+                eprintln!("Could not send to channel, with error: {:?}", err);
+            }
+        });
+
+        match receiver.recv().await {
+            Ok(Ok(())) => {}
+            Ok(Err(err)) => {
+                eprintln!("Could not download video, with error: {:?}", err);
+            }
+            Err(err) => {
+                eprintln!("Could not receive from channel, with error: {:?}", err);
+            }
+        }
+
+        let finished_dialog =
+            Builder::from_resource(&format!("{}ui/finished-dialog.ui", config::APP_IDPATH))
+                .objects()
+                .remove(0)
+                .downcast::<MessageDialog>()
+                .expect("Should be a MessageDialog");
+
+        finished_dialog.set_transient_for(Some(self));
+        finished_dialog.connect_response(|self_, _| {
+            self_.destroy();
+        });
+
+        download_dialog.destroy();
+        finished_dialog.show();
     }
 }
